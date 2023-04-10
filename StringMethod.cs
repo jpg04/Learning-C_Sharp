fb14e296ac0a610505f02c66d28ae1d4d6c9961b
Jaydeep Popat Godase <jaydeepgodase42161@gmail.com>
2023-04-10 16:01:54 +0530

    String Methodss in C#

---
+++ StringMethod.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+public class StringMethod
+{
+	public StringMethod()
+	{
+		string name = "My Name is Jaydeep Popat Godase";
+		//Console.WriteLine(name.Length);
+		//Console.WriteLine(name.ToLower());
+  //      Console.WriteLine(name.ToUpper());
+  //      Console.WriteLine(string.Concat(name,"I am not arrogant...!"));
+  //      Console.WriteLine(name+"I am not arrogant...!");
+
+		//string fname = Console.ReadLine();
+		//string rno = Console.ReadLine();
+  //      Console.WriteLine($"Your Name is {fname}. and Your Roll Number is {rno}.");
+		Console.WriteLine(name.IndexOf("Jaydeep"));
+		Console.WriteLine(name.Substring(1));
+
+
+
+
+    }
+}
