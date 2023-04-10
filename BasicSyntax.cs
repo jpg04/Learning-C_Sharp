9323666d1dfc79f40216d6daaa2b7c21c9e55786
Jaydeep Popat Godase <jaydeepgodase42161@gmail.com>
2023-04-10 15:52:22 +0530

    C# Basic Syntax

---
+++ BasicSyntax.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+public class BasicSyntax
+{
+    public BasicSyntax()
+    {
+        System.Console.WriteLine("Inside the Default Constructor");
+    }
+
+    public void DisplayBasicSyntax()
+    {
+        System.Diagnostics.Debug.WriteLine(" I am Diagnostics.Debug ");
+    }
+}
