3552f7f826e965d56782a3e721fe175e1a5c7140
Jaydeep Popat Godase <jaydeepgodase42161@gmail.com>
2023-04-10 16:02:52 +0530

    Math Class in C#

---
+++ MathClassinCS.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class MathClassinCS
+{
+	public MathClassinCS()
+    {
+        Console.WriteLine("Math Class in C#...!");
+
+        int a= Math.Max(21, 233);
+        int b= Math.Min(21, 233);
+        Double c= Math.Sqrt(81);
+        int  d= Math.Abs(-81);
+        Console.WriteLine("Maxomim value is : "+a);
+        Console.WriteLine("Minimum value is : "+b);
+        Console.WriteLine("Square Root of Given value is : "+c);
+        Console.WriteLine("Absolute of Given value is : "+d);
+    }
+}
