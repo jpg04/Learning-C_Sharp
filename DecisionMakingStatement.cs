dead237a6c8e609dc6a0dbbb53b07e2650ba84cd
Jaydeep Popat Godase <jaydeepgodase42161@gmail.com>
2023-04-11 23:56:57 +0530

    Decision Making Statement in C#

--- DecisionMakingStatement.cs
+++ DecisionMakingStatement.cs
@@ -12,25 +12,22 @@ public class DecisionMakingStatement
         int  age = Convert.ToInt32(agestr);
         bool isBanned = true;
 
-        if (age <10 || isBanned)
+        if (age <10 && isBanned)
         {
             Console.WriteLine("You are child or You Are Banned...!");
         }
-
-        else if(age >= 10 && age < 18 || isBanned)
+        else if(age < 18 && isBanned)
         {
             Console.WriteLine("You are Teenager or You are Banned...!");
         }
-
-        else if(age>=18 && age < 75)
+        else if(age < 75)
         {
             Console.WriteLine("You are eligible for Drive...!");
         }
-
         else
         {
             Console.WriteLine("You are Not eligible for Drive...!");
         }
-
+       
     }
 }
