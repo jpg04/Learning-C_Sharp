362379205813c67fbe31de7c077cc3c44cf7b18f
Jaydeep Popat Godase <jaydeepgodase42161@gmail.com>
2023-04-12 12:07:42 +0530

    LoopInCSharp in C#

---
+++ LoopInCSharp.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+public class LoopInCSharp
+{
+	public LoopInCSharp()
+    {
+        Console.WriteLine("While Loop in C#...!");
+
+        Console.WriteLine("Enter The traverse Number...!");
+        int num = Convert.ToInt32(Console.ReadLine());
+
+        int i = 0;
+        while (i < num)
+        {
+            Console.WriteLine(i);
+            i++;
+        }
+
+
+    }
+}
