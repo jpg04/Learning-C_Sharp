0636e1e773ad40252523338fe3ad0af168fe4c79
Jaydeep Popat Godase <jaydeepgodase42161@gmail.com>
2023-04-12 12:04:35 +0530

    ForLoopInCSharp in C#

---
+++ ForLoopInCSharp.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class ForLoopInCSharp
+{
+
+    public ForLoopInCSharp()
+    {
+        Console.WriteLine("For Loop in C Sharp...!");
+
+        Console.WriteLine("Enter Any Random Number You want to traverse : ");
+        int num = Convert.ToInt32(Console.ReadLine());
+
+        for(int i = 0; i < num; i++)
+        {
+            Console.WriteLine(i+1);
+        }
+    }
+}
