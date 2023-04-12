a3e58acc697e539de6bf65cab08435615440d36d
Jaydeep Popat Godase <jaydeepgodase42161@gmail.com>
2023-04-12 13:06:44 +0530

    MethodsInCSharp in C#

---
+++ MethodsInCSharp.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+public class MethodsInCSharp
+{
+	public MethodsInCSharp()
+	{
+        Console.WriteLine("Methods In CSharp...!");
+    }
+
+    public static void Greet()
+    {
+        Console.WriteLine("Good AfterNoon JP...!");
+    }
+
+    public static void Greet(string name) 
+    {
+        Console.WriteLine("My Name is : "+name);
+    }
+
+    public static float Average(int i, int j, int k)
+    {
+        float sum = i+ j + k;
+        return sum/3;
+    }
+
+
+}
