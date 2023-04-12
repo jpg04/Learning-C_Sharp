b0397d468805e943c6ad0571581dd93c7674efbd
Jaydeep Popat Godase <jaydeepgodase42161@gmail.com>
2023-04-12 12:06:56 +0530

    SwitchCase in C#

---
+++ SwitchCase.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+public class SwitchCase
+{
+	public SwitchCase()
+    {
+        Console.WriteLine("switch Case in C#");
+
+        Console.WriteLine("Enter Your Standard : ");
+        int std = Convert.ToInt32 (Console.ReadLine());
+        
+        switch(std)
+        {
+            case 1:
+                Console.WriteLine("You are in 1st Std...!");
+                break;
+            case 2:
+                Console.WriteLine("You are in 2nd Std...!");
+                break;
+            case 3:
+                Console.WriteLine("You are in 3rd Std...!");
+                break;
+            default:
+                Console.WriteLine("InValid Input...!");
+                break;
+        }
+    }
+}
