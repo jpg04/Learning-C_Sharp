15cd36bf7a8f907488a221061c43a8bc1668d40a
Jaydeep Popat Godase <jaydeepgodase42161@gmail.com>
2023-04-12 10:55:24 +0530

    IfElseLadderInLogicalOperator in C#

---
+++ IfElseLadderInLogicalOperator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+public class IfElseLadderInLogicalOperator
+{
+	public IfElseLadderInLogicalOperator()
+	{
+        Console.WriteLine("Enter Your Age : ");
+        int age = Convert.ToInt32(Console.ReadLine());
+        bool OverAged = true;
+        bool Underaged = true; 
+        
+        if (age > 75 && OverAged)
+        {
+            Console.WriteLine("You are not eligible for Driving or You are Overaged...!");
+        }
+        else if(age < 18  && Underaged) 
+        {
+            Console.WriteLine("You are teenager or You are Underaged...!");
+        }
+        else
+        {
+            Console.WriteLine("You are Eligible for Drive...!");
+        }
+    }
+}
