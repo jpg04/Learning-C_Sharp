09e05e75e19c43e685aa33374a937a4bace6d110
Jaydeep Popat Godase <jaydeepgodase42161@gmail.com>
2023-04-12 14:17:34 +0530

    Learning C# OOPS

---
+++ CShaprOOPS/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CSharpOOPS
+{
+    internal class Program
+    {
+        public static void Main(string[] tag)
+        {
+            Console.WriteLine("C# OOPS...!");
+        }
+    }
+}
