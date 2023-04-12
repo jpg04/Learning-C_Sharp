0fe547b3c67379bb8b528d451d5c93b372b60d2d
Jaydeep Popat Godase <jaydeepgodase42161@gmail.com>
2023-04-12 10:54:32 +0530

    IfElseLadder in C#

---
+++ IfElseLadder.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+public class IfElseLadder
+{
+	public IfElseLadder()
+    {
+        Console.WriteLine("Enter Your Age : ");
+        int age = Convert.ToInt32(Console.ReadLine());
+        if (age < 10)
+        {
+            Console.WriteLine("You are child...!");
+        }
+        else if (age <18 ) 
+        {
+            Console.WriteLine("You are Teenager...!");
+        }
+        else if(age > 18 && age <75)
+        {
+            Console.WriteLine("You are eligible for Driving...!");
+        }
+        else
+        {
+            Console.WriteLine("You are not eligible for Drive...!");
+        }
+    }
+}
