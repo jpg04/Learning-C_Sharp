af570a102aa3b29fd4820b9048d226f1ce7a9615
Jaydeep Popat Godase <jaydeepgodase42161@gmail.com>
2023-04-12 12:04:00 +0530

    DoWhileINCSharp in C#

---
+++ DoWhileINCSharp.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+public class DoWhileINCSharp
+{
+	public DoWhileINCSharp()
+    {
+        Console.WriteLine("do while loop in C#...!");
+
+        Console.WriteLine("Enter Any Random Number You Want to Traverse...!");
+        int num = Convert.ToInt32(Console.ReadLine());
+        int i =0;
+        do
+        {
+            Console.WriteLine(i);
+            i++;
+        }while (i<num);
+
+    }
+}
