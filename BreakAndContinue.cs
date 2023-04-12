1b4b0303215db394af1ca43c3f98ce94a7e1391e
Jaydeep Popat Godase <jaydeepgodase42161@gmail.com>
2023-04-12 12:03:24 +0530

    BreakAndContinue in C#

---
+++ BreakAndContinue.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+public class BreakAndContinue
+{
+    public BreakAndContinue()
+    {
+        Console.WriteLine("Break And Continue in C#...!");
+
+        Console.WriteLine("Enter Number For Break Statement : ");
+        int num = Convert.ToInt32(Console.ReadLine());  
+        int i = 0;
+
+        //for(i = 0; i < num; i++)
+        //{
+        //     if (i == 3)
+        //        break;
+
+        //   Console.WriteLine("Jaydeep");
+        //}
+
+        //for (i = 0; i < num; i++)
+        //{
+        //    if (i == 2)
+        //    {
+        //        continue;
+        //    }
+        //    Console.WriteLine(i);
+        //}
+    }
+}
