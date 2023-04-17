062b449d3cb8fbd6c3ba250ac87f2790159221a7
Jaydeep Popat Godase <jaydeepgodase42161@gmail.com>
2023-04-17 16:09:51 +0530

    Clearing the Concept of Encapsulation in C#

---
+++ EncapsulationInCS.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+public class EncapsulationInCS
+{ 
+	private int mobPin; //Characteristics 
+	public int getValue() //Behavior
+	{
+		return mobPin;
+	}
+	public void setValue(int mPin)
+	{
+		mobPin = mPin;	
+	}
+
+}
