523488b3dfaa1e79a8e6d963533f82e62d22b391
Jaydeep Popat Godase <jaydeepgodase42161@gmail.com>
2023-04-17 16:09:00 +0530

    clearing the Concept of Abstraction is C#

---
+++ AbstractionInCS.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class AbstractionInCS
+{
+	private void run()
+	{
+		//int a = 4;
+        Console.WriteLine("Inside run()...!");
+    }
+}
+
+class AbstractionInCSX : AbstractionInCS
+{
+	public void sun()
+	{
+        Console.WriteLine("Inside sun()...!");
+    }
+}
