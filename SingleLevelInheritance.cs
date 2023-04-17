cf1f6f99649f2052ba47c1f4c9ec864aa73697d4
Jaydeep Popat Godase <jaydeepgodase42161@gmail.com>
2023-04-17 16:11:48 +0530

    Clearing the concept of Single Level Inheritance in C#

---
+++ SingleLevelInheritance.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+public class SingleLevelInheritance
+{
+	protected int a=20, b=10, c=0;
+
+    public void Add()
+    {
+        c = a + b;
+        Console.WriteLine("Addition is : "+c);
+    }
+
+    public void Sub()
+    {
+        c=a - b;
+        Console.WriteLine("Subtraction is : " + c);
+    }
+}
+
+class SingleLevelInheritanceX : SingleLevelInheritance
+{
+    public void Mul()
+    {
+        c = a * b;
+        Console.WriteLine("Multiplication is : " + c);
+    }
+
+    public void Div()
+    {
+        c=a/ b;
+        Console.WriteLine("Division is : " + c);
+    }
+}
