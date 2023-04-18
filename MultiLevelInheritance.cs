92005cbd20d4bd9ea38c514b9b9a2f1abd122bca
Jaydeep Popat Godase <jaydeepgodase42161@gmail.com>
2023-04-18 23:54:20 +0530

    Clearing the concept of MultiLevel Inheritance in C#

---
+++ MultiLevelInheritance.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+public class MultiLevelInheritance
+{
+	protected int a = 30, b=20, c = 0;
+	public void Add()
+	{
+		c = a + b;
+        Console.WriteLine("Addition is : "+c);
+    }
+
+	public void Sub()
+	{
+		c = a - b;
+        Console.WriteLine("Subtraction is : "+c);
+    }
+}
+
+class MultiLevelInheritanceX : MultiLevelInheritance
+{
+	public void Mul()
+	{
+		c = a * b;
+        Console.WriteLine("Multiplication is : "+c);
+    }
+
+	public void Div()
+	{
+		c= a / b;
+		Console.WriteLine("Division is : " + c);
+	}
+}
+
+class MultiLevelInheritanceXX : MultiLevelInheritanceX
+{
+	public void Mod()
+	{
+		c = a % b;
+		Console.WriteLine("Modulo is : " + c);
+	}
+}
