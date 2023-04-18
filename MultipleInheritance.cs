a1dbcbf0bcb099f23f8db897321d3f9881522fc7
Jaydeep Popat Godase <jaydeepgodase42161@gmail.com>
2023-04-19 00:31:08 +0530

    Clearing the concept of Multiple Inheritance in Csharp

---
+++ MultipleInheritance.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+interface X
+{
+	void Add();  //by default public
+}
+
+interface XX
+{
+	void Sub(); // by default public
+}
+public class MultipleInheritance : X,XX
+{
+	public void Add()
+	{
+		int a = 10, b = 20;
+        Console.WriteLine(a+b);
+    }
+	public void Sub()
+	{
+		int a = 10, b = 20;
+		Console.WriteLine(a-b);
+	}
+}
+
+
