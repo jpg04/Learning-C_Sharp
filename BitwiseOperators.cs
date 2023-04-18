cad2bef1be5e84ef6ebb77c925d9fd6624a90d60
Jaydeep Popat Godase <jaydeepgodase42161@gmail.com>
2023-04-18 23:51:00 +0530

    Bitwise operators in CSharp

---
+++ BitwiseOperators.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public class BitwiseOperators
+{
+	public BitwiseOperators()
+	{
+		Console.WriteLine("Bitwise Operator in C#...!");
+
+		int a = 5, b = 10, result;
+
+		//Bitwise AND Operator
+		result = a & b;
+		Console.WriteLine("Bitwise AND : " + result);
+		result = a | b;
+		Console.WriteLine("Bitwise OR : " + result);
+	}
+}
