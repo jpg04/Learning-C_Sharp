9d003dece5387f22225f37530f2413df76a8ab32
Jaydeep Popat Godase <jaydeepgodase42161@gmail.com>
2023-04-19 00:31:52 +0530

    Clearing the concept of Hierarchical Inheritance in Csharp

---
+++ HierarchicalInheritance.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+
+class Base
+{
+	public int a = 20, b = 10, c=0;
+	public void Add()
+	{
+		int c = a + b;
+        Console.WriteLine("Addition is : "+c);
+    }
+}
+
+class HierarchicalInheritance : Base
+{
+	public void sub()
+	{
+		c = a - b;
+		Console.WriteLine("Subtraction is : " + c);
+	}
+}
+class HierarchicalInheritance1 : Base
+{
+	public void mul()
+	{
+		c=a*b;
+        Console.WriteLine("Multiplication is : "+c);
+    }
+}
