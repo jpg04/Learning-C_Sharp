b48cbb557129cef18b8673fe939f8b9d64a1fdc8
Jaydeep Popat Godase <jaydeepgodase42161@gmail.com>
2023-04-24 11:59:20 +0530

    Clearing the concept of Method Parameters in C#

---
+++ MethodParameters.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+public class MethodParameters
+{
+	public void MyMethod(string name, int age)
+	{
+        Console.WriteLine("My Name is : "+name + " And My age is : "+age);
+    }
+}
