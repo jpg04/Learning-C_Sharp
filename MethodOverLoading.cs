8b35af1c45610af6cda083383ec91cab0e2dcd90
Jaydeep Popat Godase <jaydeepgodase42161@gmail.com>
2023-04-24 12:00:13 +0530

    Clearing the concept of Method Overloading in C#

---
+++ MethodOverLoading.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class MethodOverLoading
+{
+	public int Addition(int a, int b)
+	{
+        return a + b;
+    }
+    public float Addition(float a, float b, float c)
+	{
+        return a + b + c;
+    }
+    public double Addition(double a, double b, double c,double d)
+	{
+        return a + b + c + d;
+    }
+
+}
