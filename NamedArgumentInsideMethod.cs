280ed6ecc145a9b8a484024b160283f9195b00b1
Jaydeep Popat Godase <jaydeepgodase42161@gmail.com>
2023-04-24 12:01:36 +0530

    Clearing the concept of Named Arguments inside the method in C#...!

---
+++ NamedArgumentInsideMethod.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+public class NamedArgumentInsideMethod
+{
+	public void NameArgument(string child1, string child2)
+	{
+        Console.WriteLine("The Youngest Child is : " +child2);
+    }
+}
