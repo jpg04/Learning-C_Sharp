bcdce51b148786c8f684a50b79ed8fc560c33306
Jaydeep Popat Godase <jaydeepgodase42161@gmail.com>
2023-04-26 08:27:56 +0530

    Clearing the concept of Array to Funtion...!

---
+++ ArrayToFunction.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+public class ArrayToFunction
+{
+    /* public void printArray(int[] Arr)
+    {
+        //Console.WriteLine("Printing Array Elements : ");
+        //for (int i = 0; i < Arr.Length; i++)
+        //{
+        //    Console.WriteLine(Arr[i]);
+        //}
+    }
+    */
+
+    /*
+     * public void printMin(int[] Arr)
+    {
+        Console.WriteLine("!...Function for Printing the minimum element in present array...!");
+
+        int min = Arr[0];
+
+        for (int i = 0; i < Arr.Length; i++)
+        {
+            if (min > Arr[i])
+            {
+                min = Arr[i];
+            }
+        }
+        Console.WriteLine("Minimum element is : "+min);
+    }
+    */
+
+    /*
+    public void printMax(int[] Arr)
+    {
+        Console.WriteLine("!...Function for Printing the Maximum element in present array...!");
+
+        int max = Arr[0];
+
+        for (int i = 0; i < Arr.Length; i++)
+        {
+            if (max < Arr[i])
+            {
+                max = Arr[i];
+            }
+        }
+        Console.WriteLine("Minimum element is : " + max);
+    }
+    */
+    
+
+}
