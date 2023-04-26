7cfaa5dd4285673a92ba3de7ce8bb3028b9777a8
Jaydeep Popat Godase <jaydeepgodase42161@gmail.com>
2023-04-26 08:33:52 +0530

    Clearing the concept MultiDimensional Array...!

---
+++ MultiDimensionalArray.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public class MultiDimensionalArray
+{
+	public void MDArray()
+	{
+		int[,] arr = new int[3, 3];
+
+		arr[0,1] = 20;
+		arr[1,2] = 30;
+		arr[2,0]= 50;
+
+		for(int i=0; i<3; i++)
+		{
+			for(int j=0; j<3; j++)
+			{
+				Console.Write(arr[i, j] + "\t");
+            }
+            Console.WriteLine();
+        }
+	}
+}
