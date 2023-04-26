0563ad429612eb9c29e4a47450c429aeff497c40
Jaydeep Popat Godase <jaydeepgodase42161@gmail.com>
2023-04-26 08:31:11 +0530

    Clearing the concept of One Dimensional Array...!

---
+++ OneDArray.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+public class OneDArray
+{
+    public OneDArray()
+    {
+        /*int[] arr = new int[3];
+        arr[0] = 10;
+        arr[1] = 20;
+        arr[2] = 30;
+
+        for(int i = 0; i < arr.Length; i++)
+        {
+            Console.WriteLine(arr[i] = i);
+        }
+        */
+
+        //Declaring and Initializing array at the same time
+
+        /*int[] arr = new int[] { 1, 2, 3,4,5,6,7,8,9 };
+        int[] b = new int[] {10,20,30,40,50,60,70,80,90};
+
+        for (int i = 0; i < arr.Length; i++)
+        {
+            Console.WriteLine(arr[i]=i);
+        }
+
+        for(int i = 0; i<b.Length; i++)
+        {
+            Console.WriteLine(b[i]);
+        }
+        */
+
+        /*
+        //using foreach loop
+        int[] arr = new int[] { 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22 };
+
+        foreach(int i in arr)
+        {
+            Console.WriteLine(i);
+        }
+        */
+
+        
+
+
+
+
+    }
+}
