8b5bb3e64f425c7dfed37824bb158dc0c6086a2f
Jaydeep Popat Godase <jaydeepgodase42161@gmail.com>
2023-04-28 22:26:15 +0530

    Clearing the concept of Jagged Array in Csharp...!

---
+++ JaggedArray.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+public class JaggedArray
+{
+	public void JaggedArrayX()
+	{
+		int[][] Arr = new int[4][] 
+		{ 
+			new int[] { 11, 12, 13, 14, 15 },
+			new int[] { 21,22,23,24},
+			new int[] { 31,32,33},
+			new int[] {41,42}
+		};
+
+		for(int i=0; i<Arr.Length; i++)
+		{
+			for(int j=0; j < Arr[i].Length; j++)
+			{
+				Console.Write(Arr[i][j]+"\t");
+            }
+            Console.WriteLine();
+        }
+
+	}
+}
