5dd21fcf67f9dadd10ce18510056d5a654bd30d6
Jaydeep Popat Godase <jaydeepgodase42161@gmail.com>
2023-04-28 22:25:33 +0530

    Clearing the concept of Params in Csharp...!

---
+++ CsharpParams.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+public class CsharpParams
+{
+	/* public void ShowParams(params int[] val)
+	{
+		for(int i=0; i<val.Length; i++) 
+		{
+			Console.WriteLine(val[i]);
+        }
+	}
+	*/
+
+	public void ShowParamsObjetcs(params object[] items)
+	{
+		for (int i=0; i<items.Length; i++)
+		{
+			Console.WriteLine(items[i]);
+		}
+	}
+}
