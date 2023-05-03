a3b03891eb6f0e56699abb648153b5bc89ed327f
Jaydeep Popat Godase <jaydeepgodase42161@gmail.com>
2023-05-03 16:42:51 +0530

    Clearing the concept of static keyword...!

---
+++ StaticKeyword.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public class StaticKeyword
+{
+	public int accNo;
+	public String name;
+	public static int count = 0;
+	public static float rateOfInterest = 8.8f;
+	public StaticKeyword(int accNo, String name)
+	{
+		this.accNo = accNo;	
+		this.name = name;
+		count++;
+	}
+
+	public void Display()
+	{
+        Console.WriteLine("Account Number is :"+accNo+"\nName is :"+name+ "\nRate of Interest is :"+rateOfInterest);
+    }
+}
