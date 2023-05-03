f9e9b00fe537274a348c385cafaff2653c9b2b10
Jaydeep Popat Godase <jaydeepgodase42161@gmail.com>
2023-05-03 16:43:21 +0530

    Clearing the concept of this keyword...!

---
+++ ThisKeyword.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+public class ThisKeyword
+{
+	public ThisKeyword()
+	{
+        Console.WriteLine("Default Constructor....!");
+    }
+
+    public int id;
+    public string name;
+    public float salary;
+    public ThisKeyword(int id, string name, float salary)
+    {
+        this.id = id;
+        this.name = name;
+        this.salary = salary;
+    }
+
+    public void Display()
+    {
+        Console.WriteLine(id+" "+name +" "+salary);
+    }
+}
+
