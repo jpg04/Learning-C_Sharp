5ba316f32e51058931adff1ec36042bc233eaae7
Jaydeep Popat Godase <jaydeepgodase42161@gmail.com>
2023-05-04 22:48:52 +0530

    Clearing the concept of Static constructor

---
+++ StaticConstructor.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+public class StaticConstructor
+{
+	public int id;
+	public string name;
+	public static float rateOfInterest;
+	public StaticConstructor(int id, string name)
+	{
+		this.id = id;
+		this.name = name;
+	}
+
+	static StaticConstructor()
+	{
+		rateOfInterest = 9.5f;
+	}
+
+	public void Display()
+	{
+        Console.WriteLine("ID is : "+id+"\nName is : "+name+"\nRate of Interest is :"+rateOfInterest);
+    }
+}
