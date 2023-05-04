ecd9f8008187819faa93bac252261f674be9c27c
Jaydeep Popat Godase <jaydeepgodase42161@gmail.com>
2023-05-04 22:34:16 +0530

    Clearing the concept of Static class

---
+++ StaticClass.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public static class StaticClass
+{
+	public static float PI = 3.14f;
+	public static int Cube(int n)
+	{
+		return n * n * n;
+	}
+	//public StaticClass()
+	//{
+	//}
+}
