d07cfdfb139e9e00d96a055deeadf7aec15651b1
Jaydeep Popat Godase <jaydeepgodase42161@gmail.com>
2023-05-08 12:59:21 +0530

    Clearing the concept of Aggregation is C#

---
+++ AggregationInCS.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+public class Address
+{
+	public string addressLine, city, state;
+	public Address(string addressLine, string city, string state)
+	{
+		this.addressLine = addressLine;
+		this.city = city;
+		this.state = state;
+	}
+}
+public class AggregationInCS
+{
+	public int id;
+	public string name;
+	public Address address; // Employee HAS-A Address
+	public AggregationInCS(int id, string name, Address address)
+	{
+		this.id = id;
+		this.name = name;
+		this.address = address;
+	}
+
+	public void Display()
+	{
+        Console.WriteLine(id+" "+name+" "+address.addressLine+" "+address.city+" "+address.state);
+    }
+}
