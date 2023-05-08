8589f013172ccd5933264600a416a44bfeb6f273
Jaydeep Popat Godase <jaydeepgodase42161@gmail.com>
2023-05-08 16:31:40 +0530

    Clearing the concept of Method Overriding in C#

---
+++ MethodOverriding.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+public class MethodOverriding
+{
+	public virtual void Fun()
+	{
+        Console.WriteLine("Inside the Base Fun...!");
+    }public virtual void Gun()
+	{
+        Console.WriteLine("EaInside the Base Gun...!");
+    }public virtual void Sun()
+	{
+        Console.WriteLine("Inside the Base Sun...!");
+    }
+}
+
+public class DerivedMethodOverriding : MethodOverriding
+{
+    public override void Gun()
+    {
+        Console.WriteLine("Inside the Derived Gun...!");
+    }
+    public override void Sun()
+    {
+        Console.WriteLine("Inside the Derived Gun...!");
+    }
+    //public override void Run()
+    //{
+    //    Console.WriteLine("Inside the Derived Run...!");
+    //}
+}
+
