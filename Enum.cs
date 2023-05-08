88771ad36ec3a61af0c03b3795d6134560daad6b
Jaydeep Popat Godase <jaydeepgodase42161@gmail.com>
2023-05-08 10:59:55 +0530

    Concept of Enum in C#

---
+++ Enum.cs
@@ -0,0 +1,24 @@
+﻿using System;
+/*
+public class Enum
+{
+    public enum Season { WINTER, SPRING, SUMMER, FALL }
+    public enum SeasonS { WINTER = 10, SPRING, SUMMER, FALL }
+
+    public static void Main(string[] args)
+    {
+        int x = (int)Season.WINTER;
+        int y = (int)Season.SUMMER;
+        Console.WriteLine("WINTER = {0}", x);
+        Console.WriteLine("SUMMER = {0}", y);
+        Console.WriteLine();
+        int j = (int)SeasonS.WINTER;
+        int p = (int)SeasonS.SUMMER;
+        Console.WriteLine("WINTER = {0}", j);
+        Console.WriteLine("SUMMER = {0}", p);
+    }
+
+
+}
+
+*/
