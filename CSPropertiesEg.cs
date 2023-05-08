b8a08102f83bf452c0a42e0e211526763449f5e5
Jaydeep Popat Godase <jaydeepgodase42161@gmail.com>
2023-05-08 11:44:23 +0530

    Clearing the concept of C# Properties

---
+++ CSPropertiesEg.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Diagnostics.Metrics;
+
+public class CSPropertiesEg
+{
+	//private String name;//
+
+	//public String Name
+	/*{
+		get
+		{
+			return name;
+		}
+
+		set
+		{
+			name = value;
+		}
+	}
+	*/
+
+	/*{
+		get
+		{
+			return name;
+		}
+		set
+		{
+			name = value+ "Patil";
+		}
+	}
+	*/
+
+	private static int iCnt;
+
+	public CSPropertiesEg()
+	{
+        iCnt++;
+	}
+	
+	public static int Counter
+	{
+		get
+		{
+			return iCnt;	
+		}
+	}
+}
