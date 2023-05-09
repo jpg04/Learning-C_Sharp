f167f5196ad33a5d5c4d8b7de631929e1e588a4d
Jaydeep Popat Godase <jaydeepgodase42161@gmail.com>
2023-05-09 12:42:51 +0530

    Clearing the concept of Base Keyword in C#

---
+++ BaseKeyword.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+public class BaseKeyword
+{
+    public string Name = "Godase";
+	
+	public void Cricket()
+	{
+        Console.WriteLine("Bowling All-Rounder");
+    }
+    
+}
+
+public class BaseKeywordChild : BaseKeyword
+{
+    string Name = "Jaydeep";
+
+    public void showName()
+    {
+        base.Cricket();
+        Console.WriteLine(base.Name);
+        Console.WriteLine(Name);
+    }
+
+
+}
