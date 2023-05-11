7d1de071374c521efbe289cc1fe707bee586cd67
Jaydeep Popat Godase <jaydeepgodase42161@gmail.com>
2023-05-11 16:20:28 +0530

    Clearing the concept of RunTime PolymorPhism...!

---
+++ RunTimePolymorphism.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class BaseRunTimePolymorphism
+{
+	public virtual void Run()
+	{
+		Console.WriteLine("Inside Run()...!"); ;
+    }
+}
+class RunTimePolymorphism : BaseRunTimePolymorphism
+{
+	public override void Run()
+	{
+		Console.WriteLine("Inside Run()...!");
+        Console.WriteLine("Runtime polymorphism achieved...!");
+    }
+}
