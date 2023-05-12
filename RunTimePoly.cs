85cfebeab2f0da077010768503ee23ebb5a32a66
Jaydeep Popat Godase <jaydeepgodase42161@gmail.com>
2023-05-12 15:49:17 +0530

    RunTime polymorphism having two derived classes

---
+++ RunTimePoly.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+public class RunTimePoly
+{
+	public virtual void draw()
+	{
+        Console.WriteLine("Drawing Something");
+    }
+}
+
+public class Rectangles : RunTimePoly
+{
+    public override void draw()
+    {
+        Console.WriteLine("Drawing Rectangles");
+    }
+}
+
+public class Circle : RunTimePoly
+{
+    public override void draw()
+    {
+        Console.WriteLine("Drawing Circle");
+    }
+}
+
+
+//Another example of runtime polymorphism in C# where we are having two derived classes.
