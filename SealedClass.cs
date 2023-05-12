6ef56bd9bed6bcb9415176fd1bf418cacd81edc3
Jaydeep Popat Godase <jaydeepgodase42161@gmail.com>
2023-05-12 16:21:08 +0530

    Concept of Sealed Class

---
+++ SealedClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+sealed public class SealedClass
+{
+	public void Eat()
+	{
+        Console.WriteLine("Eat Something");
+    }
+}
+
+public class SealedDerived : SealedClass
+{
+    public void Eating()
+    {
+        Console.WriteLine("I am Eating");
+    }
+}
