fa435ab48a7afc27ee20739f201be20649641f9a
Jaydeep Popat Godase <jaydeepgodase42161@gmail.com>
2023-05-12 16:40:08 +0530

    Concept of Sealed method

---
+++ SealedMethod.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+public class SealedMethod
+{
+	public virtual void eat()
+    {
+        Console.WriteLine("Eating");
+    }
+
+    public virtual void Run() 
+    {
+        Console.WriteLine("Running");
+    }
+}
+
+public class SealedMethodDerived : SealedMethod
+{
+    public override void eat()
+    {
+        Console.WriteLine("Eating something");
+    }
+    public sealed override void Run() 
+    {
+        Console.WriteLine("I Am Running State");
+    }
+}
+public class SealedMethodDerived2 : SealedMethodDerived
+{
+    public override void eat()
+    {
+        Console.WriteLine("Eating something");
+    }
+    public override void Run() 
+    {
+        Console.WriteLine("I Am Running State inside Sealed 2");
+    }
+}
+
+
