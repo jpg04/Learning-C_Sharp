6b785e3cec14e052b67ea0dc0d96cd30008d18d2
Jaydeep Popat Godase <jaydeepgodase42161@gmail.com>
2023-05-15 12:40:51 +0530

    Concept of abstract keyword....@

---
+++ AbstractionUsingAbstractKeyword.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+abstract class AbstractionUsingAbstractKeyword
+{ 
+    public abstract void run();
+
+    public void sleep()
+    {
+        Console.WriteLine("I am Sleeping...!");
+    }
+}
+
+class AbstractionUsingAbstractKeywordX : AbstractionUsingAbstractKeyword
+{
+    public override void run()
+    {
+        Console.WriteLine("I am Running...!");
+    }
+    public void sun()
+    {
+        Console.WriteLine("Inside sun()...!");
+    }
+}
