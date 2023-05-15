0b83ecd043f10fc857ae0dc8ea2d923309ff9b02
Jaydeep Popat Godase <jaydeepgodase42161@gmail.com>
2023-05-15 08:53:42 +0530

    Conncept of Interface in C#...!

---
+++ InterfaceInCS.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public interface InterfaceInCS
+{
+    void Draw();
+}
+
+public class InterfaceInCS2 : InterfaceInCS
+{
+    public void Draw()
+    {
+        Console.WriteLine("Drawing Rectangle...!");
+    }
+}
+
+public class InterfaceInCS3 : InterfaceInCS
+{
+    public void Draw()
+    {
+        Console.WriteLine("Drawing Circle...!");
+    }
+}
