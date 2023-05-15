8ee5a20db5fcc0d299db34967bc4434b718bb8a6
Jaydeep Popat Godase <jaydeepgodase42161@gmail.com>
2023-05-15 08:45:42 +0530

    Concept of Abstract class and abstract method...!

---
+++ AbstractClass.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public abstract class AbstractClass
+{
+	public abstract void Draw();
+}
+
+public class AbstractClass2 : AbstractClass
+{
+	public override void Draw()
+	{
+        Console.WriteLine("Drawing Rectangle...!");
+    }
+}
+
+public class AbstractClass3 : AbstractClass2
+{
+    public override void Draw()
+    {
+        Console.WriteLine("Drawing Circle...!");
+    }
+}
