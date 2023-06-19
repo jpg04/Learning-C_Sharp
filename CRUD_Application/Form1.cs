b2a735979ec480c873ae01b5adac5a0e9f6693f4
Jaydeep Popat Godase <jaydeepgodase42161@gmail.com>
2023-06-19 13:08:12 +0530

    Code for CRUD Application

--- CRUD_Application/Form1.cs
+++ CRUD_Application/Form1.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+
 
 namespace CRUD_Application
 {
@@ -25,6 +27,65 @@ namespace CRUD_Application
         private void Form1_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection("Data Source=JPG\\SQLEXPRESS;Initial Catalog=crudApp;Integrated Security=True");
+            con.Open();
+            SqlCommand cmd = new SqlCommand("insert into userTable values(@id, @name, @email)",con);
+            cmd.Parameters.AddWithValue("@id", int.Parse(textBox1.Text));
+            cmd.Parameters.AddWithValue("@name", textBox2.Text);
+            cmd.Parameters.AddWithValue("@email", textBox3.Text);
+
+            cmd.ExecuteNonQuery();
+            con.Close();
+
+            MessageBox.Show("Data Inserted Successfully");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection("Data Source=JPG\\SQLEXPRESS;Initial Catalog=crudApp;Integrated Security=True");
+            con.Open();
+
+            SqlCommand cmd = new SqlCommand("update userTable set name=@name, email=@email where id=@id",con);
+            cmd.Parameters.AddWithValue("@id", int.Parse(textBox1.Text));
+            cmd.Parameters.AddWithValue("@name", textBox2.Text);
+            cmd.Parameters.AddWithValue("@email", textBox3.Text);
+
+            cmd.ExecuteNonQuery();
+            con.Close();
+
+            MessageBox.Show("Data Updated Successfully");
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection("Data Source=JPG\\SQLEXPRESS;Initial Catalog=crudApp;Integrated Security=True");
+            con.Open();
+
+            SqlCommand cmd = new SqlCommand("delete userTable where id=@id", con);
+            cmd.Parameters.AddWithValue("@id", int.Parse(textBox1.Text));
+
+            cmd.ExecuteNonQuery();
+            con.Close();
+
+            MessageBox.Show("Data Deleted Successfully");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection("Data Source=JPG\\SQLEXPRESS;Initial Catalog=crudApp;Integrated Security=True");
+            con.Open();
+
+            SqlCommand cmd = new SqlCommand("select * from userTable where id=@id", con);
+            cmd.Parameters.AddWithValue("@id", int.Parse(textBox1.Text));
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+
+            da.Fill(dt);
+
+            dataGridView1.DataSource = dt;
         }
     }
 }
