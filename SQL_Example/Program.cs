0244406d084b2bd18790550dc35f1dd22a913254
Jaydeep Popat Godase <jaydeepgodase42161@gmail.com>
2023-06-26 11:48:46 +0530

    Basic Example of SQL Connection with C#

---
+++ SQL_Example/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SQL_Example
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionString = @"Data Source = JPG\SQLEXPRESS; Initial Catalog = Example; User ID =JaydeepGodase; Password=; Trusted_Connection=Yes;";
+
+            SqlConnection connection = new SqlConnection(connectionString);
+            connection.Open();
+            Debug.WriteLine("Connected to the Server");
+            SqlCommand cmd = connection.CreateCommand();    
+            cmd.Connection = connection;    
+            cmd.CommandType=CommandType.Text;
+            cmd.CommandText = "Select * from dbo.people";
+            SqlDataReader reader = cmd.ExecuteReader();
+
+            if (reader.HasRows)
+            {
+                while (reader.Read())
+                {
+                    Debug.WriteLine(reader.GetString(1)+"-"+reader.GetString(2));
+                }
+            }
+
+            connection.Close();
+        }
+    }
+}
