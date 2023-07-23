0e83a953d86e55e6a8c2c5af1ed4a88f71e53cc2
Jaydeep Popat Godase <jaydeepgodase42161@gmail.com>
2023-07-23 19:05:09 +0530

    CRUD Entiry Framework Core Application

---
+++ CurdEntityFramework/App.xaml.cs
@@ -0,0 +1,38 @@
+﻿using CurdEntityFramework.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace CurdEntityFramework
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private IServiceProvider serviceProvider;
+        public App()
+        {
+            ServiceCollection services = new ServiceCollection();
+            services.AddDbContext<ProductDbContext>(option=>
+            {
+                option.UseSqlite("Data Source = Product.db");
+            }
+            );
+            services.AddSingleton<MainWindow>();
+            serviceProvider = services.BuildServiceProvider();
+
+        }
+        private void OnStartup(object s, StartupEventArgs e)
+        {
+            var mainWindow=serviceProvider.GetService<MainWindow>();
+            mainWindow.Show();
+        }
+    }
+}
---
+++ CurdEntityFramework/Data/ProductDbContext.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CurdEntityFramework.Data
+{
+    public class ProductDbContext:DbContext
+    {
+        public ProductDbContext(DbContextOptions<ProductDbContext> options) : base(options) 
+        {
+            Database.EnsureCreated();
+        }
+        public DbSet<Product> Products { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>().HasData(GetProducts());
+            base.OnModelCreating(modelBuilder);
+        }
+
+        private Product[] GetProducts()
+        {
+            return new Product[]
+            {
+                new Product { Id = 1,Name="TShirt",Description="Red",Price=1998,Unit=5},
+                new Product { Id = 2,Name="TShirt",Description="White",Price=500,Unit=5},
+                new Product { Id = 3,Name="Shirt",Description="Formal Shirt",Price=100,Unit=5}
+            };
+        }
+    }
+}
